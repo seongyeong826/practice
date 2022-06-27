61de2fad286ba11f281b497c66469fd90e048dd2
seongyeong826 <skp0826@gmail.com>
2022-06-27 15:38:41 +0900

    spring introduction start
    create project, Thymeleaf settings, build and run

---
+++ spring-start-introduction/src/main/java/hello/springstartintroduction/SpringStartIntroductionApplication.java
@@ -0,0 +1,13 @@
+package hello.springstartintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartIntroductionApplication.class, args);
+	}
+
+}
---
+++ spring-start-introduction/src/test/java/hello/springstartintroduction/SpringStartIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springstartintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
